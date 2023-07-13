0f583a6c9f278c61d7c3ada895b49cc482d54dde
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-07-14 08:25:01 +0900

    Express 세션 기본 동작 알아보기

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const session = require("express-session");
 
 const app = express();
 const PORT = 3000;
+
+app.use(session({ secret: "thisisnotgoodsecret" }));
+
+app.get("/viewcount", (req, res) => {
+  if (req.session.count) {
+    req.session.count += 1;
+  } else {
+    req.session.count = 1;
+  }
+  res.send(`You Have viewed this page ${req.session.count} times`);
+});
 
 app.listen(PORT, () => console.log(`Server Listen on Port ${PORT}`));
